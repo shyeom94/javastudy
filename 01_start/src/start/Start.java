fafed4b49dd52be2a7004e2d9dce2fea48b1c20b
Sanghun Yeom <s.h.yeom94@gmail.com>
2024-01-04 10:11:13 +0900

    20240104.java

---
+++ 01_start/src/start/Start.java
@@ -0,0 +1,24 @@
+package start;
+
+public class Start {
+  
+  public static void main(String[] args) {
+    
+    // single-line comment
+    
+    /*
+     *  multi-line comment
+     */ 
+    
+    /**
+     * documentation comment
+     * 작성자, 작성일자, 설명 등을 API 문서로 제공하기 위한 주석
+     */
+    
+    // 출력문
+    System.out.println(1); // 출력 후, 줄바꿈까지 해준다. 
+    System.out.print(2); // 출력 후, 줄바꿈 안한다. 출력만 한다. 
+    System.out.printf("%d", 3); // 포맷 문자를 기반으로 출력한다. 
+    
+  }
+} // {} 코드 블락 
