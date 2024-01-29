fd760290a961b99984ced0412d5a315981417fe0
Sanghun Yeom <s.h.yeom94@gmail.com>
2024-01-29 15:34:28 +0900

    [java:240129]

--- 15_jdbc/src/db/dao/UserDao.java
+++ 15_jdbc/src/db/dao/UserDao.java
@@ -3,13 +3,13 @@ package db.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-// 모듈화용
+// 모듈화
 public class UserDao { // 데이터베이스 
 
   // field
   private Connection con;
   
-  // private 메소드 (UserDao 내부에서 호출할 메소드)
+  // private 메소드 (UserDao 내부에서 호출하는 메소드)
   
   // private 하는 이유 : 외부에선 안쓴다. 
   
@@ -44,5 +44,10 @@ public class UserDao { // 데이터베이스
   }
 
   // public 메소드 (실제 기능을 담당하는 메소드)
+  // [DML] INSERT, UPDATE, DELETE - JAVA 에서는 Return 값 (반환값)이 존재한다. 몇 개 되었는지 값 리턴. 0이면 실패, 1이면 성공 
+  // JDBC - AUTO COMMIT 기본적으로 오토 커밋 
+  // [DQL] SELECT
   
+  
+
 }
